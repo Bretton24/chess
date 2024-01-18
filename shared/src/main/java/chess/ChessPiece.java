2e238ec4d59bf8658591b754c930dfde8735731d
Bretton24 <103625821+Bretton24@users.noreply.github.com>
2024-01-18 16:25:07 -0700

    added return values to get methods in ChessPiece and ChessMove classes. I also finished the constructors in both of these classes as well as adding global private variables

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -10,8 +10,11 @@ import java.util.Collection;
  * signature of the existing methods.
  */
 public class ChessPiece {
-
+    private final ChessGame.TeamColor pieceColor;
+    private final ChessPiece.PieceType type;
     public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
+        this.pieceColor = pieceColor;
+        this.type = type;
     }
 
     /**
@@ -37,7 +40,7 @@ public class ChessPiece {
      * @return which type of chess piece this piece is
      */
     public PieceType getPieceType() {
-        throw new RuntimeException("Not implemented");
+        return type;
     }
 
     /**
@@ -48,6 +51,13 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
+        if (type == PieceType.BISHOP){
+            return calcBishopMoves();
+        }
+        return new ArrayList<>();
+    }
+
+    private Collection<ChessMove> calcBishopMoves(){
         return new ArrayList<>();
     }
 }
