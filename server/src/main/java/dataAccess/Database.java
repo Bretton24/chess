2c4096123a719cee2dad4e533e39270e2450ef58
Bretton24 <103625821+Bretton24@users.noreply.github.com>
2024-03-06 13:12:03 -0700

    moved execute update to Database class so Sql classes could just inherit it

--- server/src/main/java/dataAccess/Database.java
+++ server/src/main/java/dataAccess/Database.java
@@ -2,6 +2,8 @@ package dataAccess;
 
 import java.sql.SQLException;
 
+import static java.sql.Types.NULL;
+
 public class Database implements GameDAO,UserDAO,AuthDAO{
   @Override
   public int sizeOfAuthTokens() {
@@ -33,18 +35,26 @@ public class Database implements GameDAO,UserDAO,AuthDAO{
     }
   }
 
-  public final String[] createStatements = {
-          """
-            CREATE TABLE IF NOT EXISTS  Games (
-              `gameID` int NOT NULL AUTO_INCREMENT ,
-              `whiteUsername` varchar(256),
-              `blackUsername` varchar(256),
-              `gameName` varchar(256),
-              `chessGame` TEXT DEFAULT NOT NULL,
-              PRIMARY KEY (`GameID`),
-              INDEX(gameID)
-            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
-            """
+  public int executeUpdate(String statement, Object ... params) throws DataAccessException{
+    try (var conn = DatabaseManager.getConnection()){
+      try (var ps = conn.prepareStatement(statement)){
+        for (var i = 0; i < params.length; i++){
+          var param = params[i];
+          if (param instanceof Integer p) ps.setInt(i + 1, p);
+          else if (param instanceof String p) ps.setString(i + 1, p);
+          else if (param == null) ps.setNull(i + 1, NULL);
+        }
+        ps.executeUpdate();
 
-  };
+        var rs = ps.getGeneratedKeys();
+        if (rs.next()){
+          return rs.getInt(1);
+        }
+
+        return 0;
+      }
+    }catch(DataAccessException | SQLException e){
+      throw new DataAccessException("unable to update database");
+    }
+  }
 }
--- server/src/main/java/dataAccess/SQLAuthDAO.java
+++ server/src/main/java/dataAccess/SQLAuthDAO.java
@@ -38,29 +38,6 @@ public class SQLAuthDAO extends Database{
     return null;
   }
 
-  private int executeUpdate(String statement, Object ... params) throws DataAccessException{
-    try (var conn = DatabaseManager.getConnection()){
-      try (var ps = conn.prepareStatement(statement)){
-        for (var i = 0; i < params.length; i++){
-          var param = params[i];
-          if (param instanceof Integer p) ps.setInt(i + 1, p);
-          else if (param instanceof String p) ps.setString(i + 1, p);
-          else if (param == null) ps.setNull(i + 1, NULL);
-        }
-        ps.executeUpdate();
-
-        var rs = ps.getGeneratedKeys();
-        if (rs.next()){
-          return rs.getInt(1);
-        }
-
-        return 0;
-      }
-    }catch(DataAccessException | SQLException e){
-      throw new DataAccessException("unable to update database");
-    }
-  }
-
   public final String[] createStatements = {
           """
             CREATE TABLE IF NOT EXISTS  Games (
