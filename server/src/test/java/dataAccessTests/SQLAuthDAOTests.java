65971e6213bee9370171c12777bc6b6b38f350e6
Bretton24 <103625821+Bretton24@users.noreply.github.com>
2024-03-07 14:47:54 -0700

    added tests

---
+++ server/src/test/java/dataAccessTests/SQLAuthDAOTests.java
@@ -0,0 +1,16 @@
+package dataAccessTests;
+
+import dataAccess.SQLAuthDAO;
+import org.junit.jupiter.api.Test;
+
+public class SQLAuthDAOTests {
+  SQLAuthDAO authDAO = new SQLAuthDAO();
+
+  public SQLAuthDAOTests() throws Exception {
+  }
+
+  @Test
+  public void testDeleteAuthTokens(){
+
+  }
+}
