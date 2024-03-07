c1d440684915bb6b2ad095d0f5643f2f3b43013c
Bretton24 <103625821+Bretton24@users.noreply.github.com>
2024-03-07 15:45:08 -0700

    created more tests

--- server/src/test/java/dataAccessTests/SQLGameDAOTests.java
+++ server/src/test/java/dataAccessTests/SQLGameDAOTests.java
@@ -1,10 +1,11 @@
 package dataAccessTests;
 
-import dataAccess.SQLAuthDAO;
-import dataAccess.SQLGameDAO;
+import dataAccess.*;
+import model.GameID;
+import model.GameName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class SQLGameDAOTests {
   SQLGameDAO gameDAO = new SQLGameDAO();
@@ -15,5 +16,30 @@ public class SQLGameDAOTests {
   public void testDeleteGames_Positive() throws Exception {
     gameDAO.deleteAllGames();
     assertEquals(0, gameDAO.lengthOfGames());
+  }
+
+
+  @Test
+  public void testCreateGames_Positive() throws Exception {
+    var game = gameDAO.createGame(new GameName("steve johnson"));
+    var newGame = gameDAO.getGame(game.gameID());
+    var gameID = new GameID(newGame.gameID());
+    assertTrue(game.gameID().equals(gameID.gameID()));
+  }
+  @Test
+  public void testCreateGames_Negative() throws Exception {
+    assertThrows(DataAccessException.class,() -> gameDAO.getGame(123));
+  }
+
+  @Test
+  public void testGetGames_Positive() throws Exception {
+    gameDAO.createGame(new GameName("Stevie Wonder"));
+    gameDAO.deleteAllGames();
+    assertEquals(0,gameDAO.lengthOfGames());
+  }
+
+  @Test
+  public void testGetGames_Negative() throws Exception {
+
   }
 }
