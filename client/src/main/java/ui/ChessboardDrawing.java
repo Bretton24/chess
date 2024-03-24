742c0fd8febc2a0ba97500a5b94b51a2cf8a73b2
Bretton24 <103625821+Bretton24@users.noreply.github.com>
2024-03-23 22:22:55 -0600

    added stuf

--- client/src/main/java/ui/ChessboardDrawing.java
+++ client/src/main/java/ui/ChessboardDrawing.java
@@ -16,24 +16,16 @@ public class ChessboardDrawing {
     var out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
 
     out.print(ERASE_SCREEN);
+
     drawHeaders(out);
 
     drawChessboard(out);
 
-    out.print(SET_BG_COLOR_BLACK);
-    out.print(SET_TEXT_COLOR_WHITE);
+//    out.print(SET_BG_COLOR_BLACK);
+//    out.print(SET_TEXT_COLOR_WHITE);
   }
 
-  private static void drawChessboard(PrintStream out) {
-    for (int row=0; row < BOARD_SIZE_IN_SQUARES; row++) {
-      drawRowOfSquares(out);
 
-      if (row < BOARD_SIZE_IN_SQUARES - 1) {
-        drawVerticalLine(out);
-        setBlack(out);
-      }
-    }
-  }
 
   private static void drawHeaders(PrintStream out) {
 
@@ -41,6 +33,7 @@ public class ChessboardDrawing {
 
     String[] headers = { " ","a", "b", "c", "d", "e", "f", "g", "h", " " };
     for (int boardCol = 0; boardCol < BOARD_SIZE_IN_SQUARES; ++boardCol) {
+
       drawHeader(out, headers[boardCol]);
 
       if (boardCol < BOARD_SIZE_IN_SQUARES - 1) {
@@ -50,36 +43,24 @@ public class ChessboardDrawing {
     out.println();
   }
 
-  private static void drawRowOfSquares(PrintStream out) {
+  private static void drawChessboard(PrintStream out) {
+    for (int row=0; row < BOARD_SIZE_IN_SQUARES; row++) {
 
+
+    }
+    out.println();
+  }
+
+  private static void drawRowOfSquares(PrintStream out) {
     for (int squareRow = 0; squareRow < SQUARE_SIZE_IN_CHARS; ++squareRow) {
       for (int boardCol = 0; boardCol < BOARD_SIZE_IN_SQUARES; ++boardCol) {
-        setWhite(out);
-
-        if (squareRow == SQUARE_SIZE_IN_CHARS / 2) {
-          int prefixLength = SQUARE_SIZE_IN_CHARS / 2;
-          int suffixLength = SQUARE_SIZE_IN_CHARS - prefixLength - 1;
-
-          out.print(EMPTY.repeat(prefixLength));
-//          printPlayer(out, rand.nextBoolean() ? X : O);
-          out.print(EMPTY.repeat(suffixLength));
-        }
-        else {
-          out.print(EMPTY.repeat(SQUARE_SIZE_IN_CHARS));
-        }
-
-        if (boardCol < BOARD_SIZE_IN_SQUARES - 1) {
-          // Draw right line
-//          setRed(out);
-          out.print(EMPTY.repeat(LINE_WIDTH_IN_CHARS));
-        }
-
+        if()
         setBlack(out);
       }
-
-      out.println();
     }
+    out.println();
   }
+
 
   private static void drawHeader(PrintStream out, String headerText) {
     int prefixLength = SQUARE_SIZE_IN_CHARS / 2;
@@ -98,17 +79,6 @@ public class ChessboardDrawing {
 
     setBlack(out);
   }
-  private static void drawSquare(PrintStream out, String square) {
-    setWhite(out);
-    out.print(square);
-    setBlack(out);
-  }
-
-//  private static void drawHorizontalLine(PrintStream out) {
-//    int lineLength = BOARD_SIZE * (SQUARE_SIZE_IN_CHARS + LINE_WIDTH_IN_CHARS) - LINE_WIDTH_IN_CHARS;
-//    setBlack(out);
-//    out.println(EMPTY.repeat(lineLength));
-//  }
 
   private static void drawVerticalLine(PrintStream out) {
     setBlack(out);
